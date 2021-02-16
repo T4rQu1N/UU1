383852c83d72c5d5a580b29cf7d8646817719f8a
T4rQu1N <mdavisaero@gmail.com>
2021-02-16 21:43:54 +0100

    adding animals

---
+++ test_animals2.py
@@ -0,0 +1,8 @@
+#Import classes from your brand new package
+import animals
+
+harmless_birds = animals.harmless.Birds()
+harmless_birds.printMembers()
+
+dangerous_fish = animals.dangerous.Fish()
+dangerous_fish.printMembers()
